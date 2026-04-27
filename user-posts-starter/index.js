f2cc73a6cda9ebbe7a99cdbc6499c56dcc02c6cf
JLamb04 <johnnylambiii@gmail.com>
2026-04-27 11:40:59 -0500

    new commit

--- user-posts-starter/index.js
+++ user-posts-starter/index.js
@@ -1,23 +1,25 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 console.log("Hello World!");
-async function main () { 
-   const users = await fetch ("https://jsonplaceholder.typicode.com/users") 
-   const userData = await users.json();
-   const userList el = document.querySelector(".user-list");
-   userData.map(
-   (user) => `<div class="user-card">
-     `<div class="user-card__container">
-     <h3>User's Name</h4>
-     <p><b>Email:</b> email@email.com</p>
-      <p><b>Phone:</b> 0000000000</p>
-     <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+
+async function main() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const userData = await users.json();
+  console.log(userData);
+  const userList = document.querySelector(".user-list");
+  userData
+    .map(
+      (user) => `<div class="user-card">
+     <div class="user-card__container">
+     <h3>${user.name}</h3>
+     <p><b>Email:</b> ${user.email}</p>
+      <p><b>Phone:</b> ${user.phone}</p>
+     <p><b>Website:</b> <a href="${user.website}" target="_blank">
+     ${user.website}
     </div>
-  </div>`
- const X = 
-   )
- .join("")
-   );
+  </div>`,
+    )
+    .join("");
 }
 
 main();
