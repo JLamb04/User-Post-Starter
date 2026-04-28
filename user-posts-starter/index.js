b6a36141056193b56f1cbba94695733d03adce9a
JLamb04 <johnnylambiii@gmail.com>
2026-04-28 12:18:35 -0500

    new commit

--- user-posts-starter/index.js
+++ user-posts-starter/index.js
@@ -1,27 +1,24 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
-console.log("Hello World!");
+ const userList = document.querySelector(".user-list");
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const userData = await users.json();
-  console.log(userData);
-  const userList = document.querySelector(".user-list");
-  const userHTML = userData
-    .map(
-      (user) => `<div class="user-card">
+  userList.innerHTML = userData.map((user) => userHTML(user)).join("");
+}
+
+main();
+ 
+function userHTML(user) {
+     `<div class="user-card">
      <div class="user-card__container">
      <h3>${user.name}</h3>
      <p><b>Email:</b> ${user.email}</p>
       <p><b>Phone:</b> ${user.phone}</p>
      <p><b>Website:</b> <a href="${user.website}" target="_blank">
      ${user.website}
-     </a>
+     </a><p>
     </div>
-  </div>`,
-    )
-    .join("");
-  userList.innerHTML = userHTML;
+  </div>`
 }
-
-main();
